02efa812cf64c8c46cfebc0e5b6dfca3d27cf6fe
Montiel Abello <u5162235@anu.edu.au>
2016-04-28 18:02:56 +1000

    added cube to diagram

--- Figures/tikzpoints.m
+++ Figures/tikzpoints.m
@@ -1,3 +1,14 @@
 clear all
 close all
 addpath(genpath('C:\Users\Monty Abello\Documents\Uni\2016\ENGN4718\Simulation\ObserverSimulation'))
+%%
+
+%axes and cube
+axes = [0 1 0 0; 0 0 1 0; 0 0 0 1];
+cube = 0.5*[-1    -1    -1    -1     1     1     1     1
+            -1    -1     1     1    -1    -1     1     1
+            -1     1    -1     1    -1     1    -1     1];
+R = rot(-pi/8*[-1 0 2]');
+
+(R*axes + repmat([8 2 0]',1,4))'
+%(R*cube + repmat([2 4 0]',1,8))'
