9ff7730f7900df66cf0db3b5e86195a2fc71d725
Montiel Abello <u5162235@anu.edu.au>
2016-04-28 17:23:09 +1000

    fixed weird error

--- Figures/tikzpoints.m
+++ Figures/tikzpoints.m
@@ -1,14 +1,3 @@
 clear all
 close all
 addpath(genpath('C:\Users\Monty Abello\Documents\Uni\2016\ENGN4718\Simulation\ObserverSimulation'))
-%%
-
-%axes and cube
-axes = [0 1 0 0; 0 0 1 0; 0 0 0 1];
-cube = 0.5*[-1    -1    -1    -1     1     1     1     1
-        -1    -1     1     1    -1    -1     1     1
-        -1     1    -1     1    -1     1    -1     1];
-R = rot(pi/12*[1 0 1]');
-
-%(R*axes + repmat([2 4 0]',1,4))'
-(R*cube + repmat([2 4 0]',1,8))'
